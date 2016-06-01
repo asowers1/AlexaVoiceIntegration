07da1543c98a5edc9219ed00c20d493a1da7dd30
Dan Edgar <batgar@gmail.com>
2016-06-01 06:48:27 -0500

    Initial Commit

---
+++ AlexaVoiceIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlexaVoiceIntegration
+//
+//  Created by Dan Edgar on 6/1/16.
+//  Copyright © 2016 Batgar, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
